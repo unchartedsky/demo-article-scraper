324ab4171571c1594c85603b3fbdedcede7d5ff8
Jason Park <binarydiver@outlook.com>
2023-01-07 19:48:48 +0900

    setup antd

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { ConfigProvider, theme } from "antd";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ConfigProvider
+      theme={{
+        algorithm: theme.darkAlgorithm,
+      }}
+    >
+      <Component {...pageProps} />
+    </ConfigProvider>
+  );
 }
